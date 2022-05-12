dc52370705a2daad2ffc557b6b10cbc79a0403cd
WWEMGamer2 <73326807+WWEMGamer2@users.noreply.github.com>
2022-05-12 18:23:57 +1000

    Create goobler-int.py

---
+++ goobler-int.py
@@ -0,0 +1,14 @@
+# IMPORTS:
+import os
+import sys
+import re
+import time
+import random
+import json
+import requests
+
+# PURE FUNCS:
+
+def start(tkn):
+  resp = requests.get("https://goobler.tairhyswood.repl.co/api/get_name_using_auth", {"auth": f"Bearer {tkn}"}).json()
+  print("Starting {resp['user_name']}...")
