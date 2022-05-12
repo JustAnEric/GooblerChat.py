3041eed0874ae9d656743e295fe22f2117a4da79
WWEMGamer2 <73326807+WWEMGamer2@users.noreply.github.com>
2022-05-12 18:32:14 +1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+  name = "goobler_api_wrapper",
+  version = "1.0",
+  packages = [],
+  package_dir = {"": "/"},
+  url = "https://goobler.imango.com.au",
+  license = "",
+  author = "Eric - GooblerTeam",
+  author_email = "ericmuzyk@icloud.com",
+  description = "The official GooblerAPI Wrapper for Goobler companion!"
+)
