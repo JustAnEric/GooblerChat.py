423192911b13f3622ec2ba225a5de434255d7cbc
WWEMGamer2 <ericmuzyk@icloud.com>
2022-06-30 13:17:59 +1000

    added stuff

---
+++ math/main.py
@@ -0,0 +1,7 @@
+async def add(nums=[]):
+    # MAX OF THREE
+    return nums.append([1]) + nums.append([2]) + nums.append([3])
+
+async def minus(nums=[]):
+    # MAX OF THREE
+    return nums.append([1]) + nums.append([2]) + nums.append([3])
