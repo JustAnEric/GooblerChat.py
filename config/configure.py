3f06f90e4d1ff0609106140ec65132c3eb3ca0bc
WWEMGamer2 <73326807+WWEMGamer2@users.noreply.github.com>
2022-07-17 20:25:32 +1000

    commited version 2.0

--- config/configure.py
+++ config/configure.py
@@ -2,8 +2,9 @@ from list import listarray
 
 class ListValueType:
     def __init__(self, value):
-        self.String = str(value)
-        self.Integer = int(value)
+        pass
+    String = str(value)
+    Integer = int(value)
 
 def get_list():
     return listarray
