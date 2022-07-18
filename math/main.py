ee9f5ab6da394c5bbf3e558f51e561345a385f91
WWEMGamer2 <73326807+WWEMGamer2@users.noreply.github.com>
2022-07-18 10:32:55 +1000

    e

--- math/main.py
+++
@@ -1,7 +0,0 @@
-async def add(nums=[]):
-    # MAX OF THREE
-    return nums.append([1]) + nums.append([2]) + nums.append([3])
-
-async def minus(nums=[]):
-    # MAX OF THREE
-    return nums.append([1]) + nums.append([2]) + nums.append([3])
