e42ba0b843ef354eee5cd7cec2ec509473aa1c76
WWEMGamer2 <73326807+WWEMGamer2@users.noreply.github.com>
2022-08-20 20:44:09 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,18 +6,18 @@ from setuptools import setup
 
 
 setup(
-  # the name must match the folder name 'verysimplemodule'
-        name="verysimplemodule", 
-        version=VERSION,
-        author="Jason Dsouza",
+  # the name must match the folder name 'gooblpy'
+        name="gooblpy", 
+        version="1.0.0",
+        author="Goobler",
         author_email="<admin@goobler.epizy.com>",
-        description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
+        description="The Goobler Python API",
+        long_description="Goobler's python API package which you may host your bot on!",
         packages=find_packages(),
         install_requires=[], # add any additional packages that 
         # needs to be installed along with your package. Eg: 'caer'
         
-        keywords=['python', 'first package'],
+        keywords=['python', 'api'],
         classifiers= [
             "Development Status :: 3 - Alpha",
             "Intended Audience :: Education",
