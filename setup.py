84fb081ba1317adc482186a1474071b6bafba4a8
WWEMGamer2 <73326807+WWEMGamer2@users.noreply.github.com>
2022-08-20 20:40:25 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,24 @@ from setuptools import setup
 
 
 setup(
-  name = "goobler_api_wrapper",
-  version = "1.0",
-  packages = [],
-  package_dir = {"": "/"},
-  url = "https://goobler.imango.com.au",
-  license = "",
-  author = "Eric - GooblerTeam",
-  author_email = "ericmuzyk@icloud.com",
-  description = "The official GooblerAPI Wrapper for Goobler companion!"
+  # the name must match the folder name 'verysimplemodule'
+        name="verysimplemodule", 
+        version=VERSION,
+        author="Jason Dsouza",
+        author_email="<admin@goobler.epizy.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
 )
