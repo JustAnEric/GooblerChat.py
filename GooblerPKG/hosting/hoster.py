ab16d9f3748fbefdc66e74e2fdbf5e745d6260de
WWEMGamer2 <ericmuzyk@icloud.com>
2023-01-05 17:02:19 +1100

    I made the package better

--- GooblerPKG/hosting/hoster.py
+++ GooblerPKG/hosting/hoster.py
@@ -20,13 +20,7 @@ class colors:
 
 
 def hostForever(bool=False, tkn=None):
-    if tkn != None:
-        resp = requests.post('https://goobler.imango.com.au/api/hosts/hostforever', {"auth": tkn})
-    else:
-        print(colors.WARNING + colors.BOLD + colors.UNDERLINE + "[GooblerAPI] You cannot run hostForever without a token! Remember, the token is the admin password to your application.")
+  resp = requests.post('https://goobl2.ericplayzyt.repl.co/api/hosts/hostforever', {"auth": tkn})
 
-def startHosting(tkn=None):
-  if tkn != None:
-    resp = requests.post('https://goobler.imango.com.au/api/hosts/start', {"auth": tkn})
-  else:
-    print(colors.WARNING + colors.BOLD + colors.UNDERLINE + "[GooblerAPI] You cannot run hostForever without a token! Remember, the token is the admin password to your application.")
+def startHosting(tkn="AutoCreatedApplication"):
+  resp = requests.post('https://goobl2.ericplayzyt.repl.co/api/hosts/start', {"auth": tkn})
