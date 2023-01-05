7f4336921dafbde2ff2de1b5d1e7f73137011ff4
WWEMGamer2 <ericmuzyk@icloud.com>
2023-01-05 17:45:17 +1100

    Edited command usages

--- gooblerpkg/Basic/__init__.py
+++ gooblerpkg/Basic/__init__.py
@@ -11,8 +11,8 @@ class ctx:
     def __init__(self):
         pass
 
-    def send_message(self, message):
-        resp = requests.get("https://goobl2.ericplayzyt.repl.co/api/messaging/send_message/")
+    def send_message(self, message, sender="My Bot"):
+        resp = requests.get("https://goobl2.ericplayzyt.repl.co/api/messaging/send_message/s/minecraft", headers={"message": message, "sent_by": sender})
 
 def command(function, **kwargs):
     return function and ctx
