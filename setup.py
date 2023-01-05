215c3c8f7a6dedafac16b03fc9bf823753d3063c
WWEMGamer2 <ericmuzyk@icloud.com>
2023-01-05 19:15:11 +1100

    change

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import find_packages
 setup(
   # the name must match the folder name 'GooblerPKG'
         name="GooblerPKG", 
-        version="2.0.26",
+        version="2.0.27",
         author="Goobler",
         author_email="<admin@goobler.epizy.com>",
         description="The Goobler Python API",
