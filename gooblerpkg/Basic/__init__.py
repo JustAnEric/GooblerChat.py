934ad82cfdc381c5f594e9b63b015c6d1d0e75a1
WWEMGamer2 <ericmuzyk@icloud.com>
2023-01-05 17:42:23 +1100

    Edited command usage for GooblerPKG.basic

--- gooblerpkg/Basic/__init__.py
+++ gooblerpkg/Basic/__init__.py
@@ -3,13 +3,16 @@
 ###################
 # Scripts made by Eric #
 
+import requests
+
 print("[Process] INITIALIZED")
 
 class ctx: 
     def __init__(self):
         pass
 
+    def send_message(self, message):
+        resp = requests.get("https://goobl2.ericplayzyt.repl.co/api/messaging/send_message/")
+
 def command(function, **kwargs):
-    commandname = function.__name__()
-    function()
-    return 
+    return function and ctx
